1f50c2d151a6475aba73fc5341006a2ff7d000f3
hakimi <hakimimdnoor90@gmail.com>
2025-09-27 15:42:58 +0800

    add config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Example rule
+    },
+  },
+];
